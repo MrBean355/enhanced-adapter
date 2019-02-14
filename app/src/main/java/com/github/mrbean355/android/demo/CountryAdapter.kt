f7ed444a8d1eb26b71f825497a71e7a8dde38a6e
Mike Johnston <mrbean355@gmail.com>
2019-02-14 22:49:46 +0200

    Update the demo app

--- app/src/main/java/com/github/mrbean355/android/demo/CountryAdapter.kt
+++ app/src/main/java/com/github/mrbean355/android/demo/CountryAdapter.kt
@@ -1,32 +1,31 @@
 package com.github.mrbean355.android.demo
 
-import android.support.v4.content.ContextCompat
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.CheckBox
 import com.github.mrbean355.android.EnhancedAdapter
 import com.github.mrbean355.android.enhancedadapter.demo.R
 
-class DemoAdapter : EnhancedAdapter<String, DemoAdapter.ViewHolder>(DiffCallbacks(), 3) {
+class CountryAdapter : EnhancedAdapter<String, CountryAdapter.ViewHolder>(DiffCallbacks(), 3) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ViewHolder(inflater.inflate(android.R.layout.simple_list_item_1, parent, false))
+        return ViewHolder(inflater.inflate(R.layout.item_country, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = getItemAt(position)
-        holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, if (isItemSelected(position)) R.color.colorAccent else android.R.color.transparent))
+        holder.checkBox.text = getItemAt(position)
+        holder.checkBox.isChecked = isItemSelected(position)
         holder.itemView.setOnClickListener {
             onItemClicked(holder.adapterPosition)
         }
     }
 
     override fun compareItems(lhs: String, rhs: String): Int {
-        return lhs.length.compareTo(rhs.length)
+        return lhs.compareTo(rhs)
     }
 
     override fun testItem(item: String, query: String): Boolean {
@@ -34,7 +33,7 @@ class DemoAdapter : EnhancedAdapter<String, DemoAdapter.ViewHolder>(DiffCallback
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView as TextView
+        val checkBox = itemView as CheckBox
     }
 
     class DiffCallbacks : DiffUtil.ItemCallback<String>() {
---
+++ app/src/main/java/com/github/mrbean355/android/demo/CountryListActivity.kt
@@ -0,0 +1,103 @@
+package com.github.mrbean355.android.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.github.mrbean355.android.enhancedadapter.demo.R
+
+class CountryListActivity : AppCompatActivity() {
+    private lateinit var adapter: CountryAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country_list)
+
+        adapter = CountryAdapter()
+        adapter.setItems(COUNTRIES)
+        adapter.setSelectedItems(listOf("Angola", "South Africa"))
+
+        findViewById<RecyclerView>(R.id.recycler_view).adapter = adapter
+    }
+
+    /**
+     * Filter the adapter's items based on the user's search query.
+     */
+    private fun onSearchQueryEntered(query: String?) {
+        adapter.filter(query)
+    }
+
+    /**
+     * Display the currently selected items in a Toast.
+     */
+    private fun onDoneClicked() {
+        val message = StringBuilder()
+        val selection = adapter.getSelectedItems()
+        selection.forEach {
+            if (message.isNotEmpty()) {
+                message.append(", ")
+            }
+            message.append(it)
+        }
+        if (selection.isEmpty()) {
+            message.append("(nothing)")
+        }
+        message.insert(0, "You selected: ")
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.country_list, menu)
+        val menuItem = menu?.findItem(R.id.action_search)
+        val searchView = menuItem?.actionView as? SearchView
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                onSearchQueryEntered(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                onSearchQueryEntered(newText)
+                return false
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_done) {
+            onDoneClicked()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private companion object {
+        private val COUNTRIES = listOf("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda",
+                "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
+                "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria",
+                "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island",
+                "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo", "The Democratic Republic of The", "Cook Islands", "Costa Rica", "Cote D'ivoire", "Croatia", "Cuba",
+                "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
+                "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon",
+                "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-bissau", "Guyana", "Haiti",
+                "Heard Island and Mcdonald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran",
+                "Islamic Republic of", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea", "Democratic People's Republic of",
+                "Korea", "Republic of", "Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein",
+                "Lithuania", "Luxembourg", "Macao", "Macedonia", "The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
+                "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Federated States of", "Moldova", "Republic of", "Monaco", "Mongolia", "Montserrat",
+                "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria",
+                "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Palestinian Territory", "Occupied", "Panama", "Papua New Guinea",
+                "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Helena",
+                "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and The Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia",
+                "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+                "South Georgia and The South Sandwich Islands", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland",
+                "Syrian Arab Republic", "Taiwan", "Province of China", "Tajikistan", "Tanzania", "United Republic of", "Thailand", "Timor-leste", "Togo", "Tokelau", "Tonga",
+                "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
+                "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Viet Nam", "Virgin Islands", "British", "Virgin Islands",
+                "U.S.", "Wallis and Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe")
+    }
+}
